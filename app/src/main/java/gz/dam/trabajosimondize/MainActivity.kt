f88f641143dd45ff5655d51f3ec616016bdc800e
dam <daapariciopereira2@danielcastelao.org>
2025-11-05 12:53:00 +0100

    Commit ViewModel: Asignacion a la MainActivity la Interfaz

--- app/src/main/java/gz/dam/trabajosimondize/MainActivity.kt
+++ app/src/main/java/gz/dam/trabajosimondize/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import gz.dam.trabajosimondize.ui.theme.TrabajoSimonDizeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TrabajoSimonDizeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+           Interfaz(ViewModel)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrabajoSimonDizeTheme {
-        Greeting("Android")
     }
 }
